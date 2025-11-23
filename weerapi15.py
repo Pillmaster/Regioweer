259588b12d4bf4201ae30aeaaec9b5bf810dd24d
Pillmaster <ezieltjes@gmail.com>
2025-11-23 15:49:13 +0100

    Add files via upload

--- weerapi15.py
+++ weerapi15.py
@@ -27,7 +27,11 @@ STATIONS = {
     "Graninge": "graninge_tv",     
     "Ärtrik": "artrik",
     "Grillom": "grillom",
+    "Bipsgarden": "fors_tv",
+    "Kramfors Norr": "kramfors_norr",
+    "Sillre": "sillre",
     "Fångsjöbacken": "fangsjobacken",
+    "Ljustorp": "ljustorp",
     "Långsele": "langsele",
     "Sollefteå/Trästa": "trasta"
     
